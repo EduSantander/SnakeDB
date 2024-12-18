2b15e4901b9c1955bf9e95a8ed4f616ad9472dc7
Edu <eduisant@espol.edu,ec>
2024-12-18 12:27:04 -0500

    habilitar cors

--- sisEmbedidos/settings.py
+++ sisEmbedidos/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'snake',
     'rest_framework',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Reemplaza con la URL de tu frontend
+]
 
 ROOT_URLCONF = 'sisEmbedidos.urls'
 
