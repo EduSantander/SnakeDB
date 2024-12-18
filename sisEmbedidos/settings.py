6a426b9070829f682ab07d1c1ba2673e53c19660
Edu <eduisant@espol.edu,ec>
2024-12-18 12:32:02 -0500

    correccion cors

--- sisEmbedidos/settings.py
+++ sisEmbedidos/settings.py
@@ -54,6 +54,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Reemplaza con la URL de tu frontend
 ]
