4d0e3ee3c5205278f15fbffa72e765abee308e40
Edu <eduisant@espol.edu,ec>
2024-12-18 12:09:08 -0500

    cors

--- sisEmbedidos/settings.py
+++ sisEmbedidos/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'snake',
     'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Reemplaza con la URL de tu frontend
 ]
 
 ROOT_URLCONF = 'sisEmbedidos.urls'
